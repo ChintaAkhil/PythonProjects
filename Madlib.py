149482bef6e18654afc49f8f4b3c7475fe613db6
CHINTA AKHIL <47230265+ChintaAkhil@users.noreply.github.com>
2020-12-16 00:24:40 -0500

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,20 @@
+Asgardian = input("Asgardian: ")
+race = input("Evil Race:  ")
+source = input("Energy Source: ")
+Famous_person = input("Famous person: ")
+lieutenant = input("Lieutenant: ")
+Agent = input("Agent: ")
+physicist = input("Physicist: ")
+Agent2 = input("Avenger: ")
+Weapon = input("Weapon: ")
+mad_lib = f"The Asgardian {Asgardian} encounters the Other, the leader of an extraterrestrial race known as the {race}." \
+          f" In exchange for retrieving the {source},[N 2] a powerful energy source of unknown potential, " \
+          f"the Other promises Loki an army with which he can subjugate Earth. {Famous_person}, director of the espionage " \
+          f"agency S.H.I.E.L.D., and his lieutenant Agent {lieutenant} arrive at a remote research facility during an " \
+          f"evacuation, where physicist {physicist} is leading a research team experimenting on the {source}. " \
+          f"Agent {Agent} explains that the object has begun radiating an unusual form of energy. The {source} " \
+          f"suddenly activates and opens a wormhole, allowing Loki to reach Earth. {Asgardian} takes the {source} and uses " \
+          f"his {Weapon} to enslave {physicist} and a few other agents, including {Agent2}, to aid him in his getaway." \
+
+
+print(mad_lib)
